feb5803aa569df335a68ef1252f35530d90a5197
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2020-07-17 23:55:16 -0300

    Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Henrique Matheus";
+var idade = 24
+var idade2 = 2
+var frase = "Japão é o melhor time do mundo."
+//alert("nome: " + nome + " tem " + idade + " anos.");
+alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"))
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
