b79c7787a3c6b9704c7fa7182b49008e3315e6c6
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2020-07-18 22:37:15 -0300

    Aula de introdução  ao JavaScript concluída

---
+++ js/main-obsoleta.js
@@ -0,0 +1,98 @@
+//var nome = "Henrique Matheus";
+//var idade = 24;
+//var idade2 = 2;
+//var frase = "Japão é o melhor time do mundo.";
+
+//lista
+//var lista = ["maça", "pêra", "laranja"];
+
+//dicionário
+//var fruta = {nome:"maçã", cor:"vermelha"};
+
+//lista de dicionários
+//var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+
+//condicionais
+//var idade = 18;
+
+//recebendo valor
+//var idade = prompt("Qual a sua idade?");
+
+//while
+//var count = 0;
+
+//for
+//var count;
+
+//Data
+/*var d = new Date();
+alert(d);
+alert(d.getMonth()+1);
+alert(d.getMinutes());
+alert(d.getDay)();*/
+
+//Funções
+/*function soma(n1, n2){
+    return n1+n2;
+};*/
+
+/*function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+    validar = true;  
+    }else{
+        validar = false;
+    };
+    return validar;
+};*/
+
+/*var idade = prompt("Qual a sua idade?")
+console.log(validaIdade(idade))*/
+
+//alert("nome: " + nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+//console.log(frase.toUpperCase());
+//console.log(frase.toLowerCase());
+
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista[1]);
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.toString()[0]);
+//console.log(lista.join(" - "));
+
+//console.log(fruta);
+//console.log(fruta.nome);
+
+//console.log(frutas);
+//console.log(frutas[1].nome);
+
+//if(idade >= 18){
+//    alert("maior de idade.");
+//}else{
+//    alert("menor de idade.");
+//};
+
+/*
+while (count < 5){
+    console.log(count);
+    //count = count + 1;
+    //ou
+    count++;
+}*/
+
+/*
+for(count = 0; count <= 5; count++){
+    alert(count);
+};*/
+
+
+//alert(soma(5,10));
--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
-var nome = "Henrique Matheus";
-var idade = 24
-var idade2 = 2
-var frase = "Japão é o melhor time do mundo."
-//alert("nome: " + nome + " tem " + idade + " anos.");
-alert(idade + idade2);
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase.replace("Japão", "Brasil"))
-console.log(frase.toUpperCase());
-console.log(frase.toLowerCase());
+function clicou(){
+    //alert("Obrigado por clicar.")
+    //document.getElementById("agradecimento");
+    //console.log(document.getElementById("agradecimento"));
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado.</b>";
+};
+
+function redirecionar(){
+    window.open("https://www.google.com");  
+};
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse.";
+    //alert("trocar texto.");
+    elemento.innerHTML = "Obrigado por passar o mouse.";
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui.";
+    elemento.innerHTML = "Passe o mouse aqui."
+}
+
+function load(){
+    alert("carregado.")
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
